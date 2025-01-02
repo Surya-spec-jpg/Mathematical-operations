09402d5be4689cfd84e2d5c1464e95c006eabedc
Surya <sxrya007@gmail.com>
2025-01-02 12:27:36 +0530

    mat.java

--- mat.java
+++ mat.java
@@ -4,20 +4,23 @@ class mat{
         int a=10;
         int b=76;
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter an operator(+,-,*,/):");
+        System.out.println("Enter an operator(+,-,*,/,%):");
         String operator=scanner.next();
         switch (operator){
             case "+":
-                System.out.println(+(a+b));
+                System.out.println("Result:"+(a+b));
                 break;
             case "-":
-                System.out.println(+(a-b));
+                System.out.println("Result:"+(a-b));
                 break;
             case "*":
-                System.out.println(+(a*b));
+                System.out.println("Result:"+(a*b));
                 break;
             case "/":
-                System.out.println(+(a/b));
+                System.out.println("Result:"+(a/b));
+                break;
+            case "%":
+                System.out.println("Result:"+(a%b));
                 break;
             default:
                 System.out.println("Enter a valid operator!");    
